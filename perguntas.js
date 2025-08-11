ea414541bb54e0a87866520eb76b0b8bdae651f8
kelwin293 <kelwin.postanovicz@escola.pr.gov.br>
2025-08-11 08:45:55 -0300

    llllhgf

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital da China?',
+    'Pequim'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a maior montanha do Mundo?',
+    'Monte Everest'
+)
+
+criaCartao(
+    'História',
+    'Quem governava o Brasil na época do Estado Novo?',
+    'Getúlio Vargas'
+)
+
+criaCartao(
+    'História',
+    'Em que ano iniciou a 1º Guerra Mundial?',
+    '1914'
+)
+
+criaCartao(
+    'Ciências',
+    'Quais são os 3 primeiros planetas do Sistema Solar?',
+    'Mercúrio, Vẽnus e Terra'
+)
+
+criaCartao(
+    'Ciências',
+    'Quais são os estados da água?',
+    'Sólido, Líquido e Gasoso'
+)
